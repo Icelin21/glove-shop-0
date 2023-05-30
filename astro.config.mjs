a47d036af542ba4fd73a9fcd90147637778fc58c
Icelin <102626367+Icelin21@users.noreply.github.com>
2023-05-29 20:47:50 -0400

    Update 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: '/my-repo',
+  site: 'https://astronaut.github.io',
+  base: '/my-repo',
+  integrations: [tailwind()]
 });
